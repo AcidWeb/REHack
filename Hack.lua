a5490cd61955843c1a0f864467ffa7d70960965b
Paweł Jastrzębski <pawelj@iosphe.re>
2019-02-14 22:55:22 +0100

    Updated libraries

--- Hack.lua
+++ Hack.lua
@@ -4,38 +4,44 @@ _G.REHack = RE
 local COMM = LibStub('AceComm-3.0')
 
 -- UIDropDownMenu taint workaround by foxlit
-if (UIDROPDOWNMENU_VALUE_PATCH_VERSION or 0) < 2 then
-	UIDROPDOWNMENU_VALUE_PATCH_VERSION = 2
-	hooksecurefunc('UIDropDownMenu_InitializeHelper', function()
-		if UIDROPDOWNMENU_VALUE_PATCH_VERSION ~= 2 then
-			return
-		end
-		for i=1, UIDROPDOWNMENU_MAXLEVELS do
-			for j=1, UIDROPDOWNMENU_MAXBUTTONS do
-				local b = _G['DropDownList' .. i .. 'Button' .. j]
-				if not (issecurevariable(b, 'value') or b:IsShown()) then
-					b.value = nil
-					repeat
-						j, b['fx' .. j] = j+1
-					until issecurevariable(b, 'value')
-				end
-			end
-		end
-	end)
-end
 if (UIDROPDOWNMENU_OPEN_PATCH_VERSION or 0) < 1 then
 	UIDROPDOWNMENU_OPEN_PATCH_VERSION = 1
-	hooksecurefunc('UIDropDownMenu_InitializeHelper', function(frame)
+	hooksecurefunc("UIDropDownMenu_InitializeHelper", function(frame)
 		if UIDROPDOWNMENU_OPEN_PATCH_VERSION ~= 1 then
 			return
 		end
 		if UIDROPDOWNMENU_OPEN_MENU and UIDROPDOWNMENU_OPEN_MENU ~= frame
-		   and not issecurevariable(UIDROPDOWNMENU_OPEN_MENU, 'displayMode') then
+		and not issecurevariable(UIDROPDOWNMENU_OPEN_MENU, "displayMode") then
 			UIDROPDOWNMENU_OPEN_MENU = nil
-			local t, f, prefix, i = _G, issecurevariable, ' \0', 1
+			local t, f, prefix, i = _G, issecurevariable, " \0", 1
 			repeat
 				i, t[prefix .. i] = i + 1
-			until f('UIDROPDOWNMENU_OPEN_MENU')
+			until f("UIDROPDOWNMENU_OPEN_MENU")
+		end
+	end)
+end
+if (COMMUNITY_UIDD_REFRESH_PATCH_VERSION or 0) < 1 then
+	COMMUNITY_UIDD_REFRESH_PATCH_VERSION = 1
+	local function CleanDropdowns()
+		if COMMUNITY_UIDD_REFRESH_PATCH_VERSION ~= 1 then
+			return
+		end
+		local f, f2 = FriendsFrame, FriendsTabHeader
+		local s = f:IsShown()
+		f:Hide()
+		f:Show()
+		if not f2:IsShown() then
+			f2:Show()
+			f2:Hide()
+		end
+		if not s then
+			f:Hide()
+		end
+	end
+	hooksecurefunc("Communities_LoadUI", CleanDropdowns)
+	hooksecurefunc("SetCVar", function(n)
+		if n == "lastSelectedClubId" then
+			CleanDropdowns()
 		end
 	end)
 end
