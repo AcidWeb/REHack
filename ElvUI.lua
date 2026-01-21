a1b88650faf8ba6f2f32c176872b811a32cf80b7
Paweł Jastrzębski <pawelj@iosphe.re>
2026-01-21 09:42:51 +0100

    New ElvUI skin

--- ElvUI.lua
+++ ElvUI.lua
@@ -1,19 +1,20 @@
-if not AddOnSkins then return end
-local AS = unpack(AddOnSkins)
-if not AS:CheckAddOn('REHack') then return end
+if not ElvUI then return end
 
-function AS:REHack()
-  AS:SkinFrame(HackListFrame)
-  AS:SkinFrame(HackEditFrame)
-  AS:SkinCloseButton(HackListFrameClose)
-  AS:SkinCloseButton(HackEditFrameClose)
-  AS:SkinCheckBox(HackSearchName)
-  AS:SkinCheckBox(HackSearchBody)
-  AS:SkinEditBox(HackSearchEdit)
-  AS:SkinScrollBar(HackEditScrollFrameScrollBar)
-  AS:SkinTab(HackListFrameTab1)
-  AS:SkinTab(HackListFrameTab2)
+local E = unpack(ElvUI)
+local S = E:GetModule('Skins')
+
+local function Skin_REHack()
+  S:HandleFrame(HackListFrame)
+  S:HandleFrame(HackEditFrame)
+  S:HandleCloseButton(HackListFrameClose)
+  S:HandleCloseButton(HackEditFrameClose)
+  S:HandleCheckBox(HackSearchName)
+  S:HandleCheckBox(HackSearchBody)
+  S:HandleEditBox(HackSearchEdit)
+  S:HandleScrollBar(HackEditScrollFrameScrollBar)
+  S:HandleTab(HackListFrameTab1)
+  S:HandleTab(HackListFrameTab2)
   HackEditBoxLineBG:SetColorTexture(0, 0, 0, 0.25)
 end
 
-AS:RegisterSkin('REHack', AS.REHack)
+S:AddCallbackForAddon('REHack', 'REHack', Skin_REHack)
